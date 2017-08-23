4e558dc6d50f6ccf103d839dcb688ee3d38b8118
Rafael Heringer <rafaheringer@live.com>
2017-08-22 23:09:44 -0300

    Disconnect function for serialComm

--- NodeProject/modules/serialComm.js
+++ NodeProject/modules/serialComm.js
@@ -13,11 +13,13 @@ function registerEvent(eventName, callback) {
 
 let eventsList = {
     dataSent: {eventName: 'dataSent', line: null},
-    connnectionError: {eventName: 'connectionError', error: null},
-    sendDataError: {eventName: 'sendDataError', error: null}
+    connectionError: {eventName: 'connectionError', error: null},
+    sendDataError: {eventName: 'sendDataError', error: null},
+    receiveData: {eventName: 'receiveData', line: null}
 };
 
 registerEvent(eventsList.sendDataError.name, ob => {console.error('SerialComm sendData: ', ob.error);});
+registerEvent(eventsList.connectionError.name, ob => {console.error('SerialComm connection: ', ob.error);});
 
 /////////////////////Connection
 function getAvailableSerialPorts() {
@@ -27,29 +29,44 @@ function getAvailableSerialPorts() {
 function connect(serialPortName, serialPortOptions){
     connectedPort = new serialPort(serialPortName, serialPortOptions);
     connectedPort.on('error', err => {
-        console.error('SerialComm connect: ', err);
-        eventEmitter.emit(eventsList.connnectionError.eventName, extendObject({error: err}, eventsList.connnectionError));
+        eventHandler.emit(eventsList.connectionError.eventName, extendObject({error: err}, eventsList.connectionError));
+    });
+
+    connectedPort.on('data', line => {
+         eventHandler.emit(eventsList.receiveData.name, extendObject({line: line}, eventsList.receiveData));
     });
 };
 
-function disconnect() {};
+function disconnect() {
+    if(connectedPort)
+        connectedPort.close(err => {
+            if(err)
+                return eventHandler.emit(eventsList.connectionError.eventName, extendObject({error: err}, eventsList.connectionError));
+
+            connectedPort = null;
+            console.log('SerialComm disconnect: connection closed');
+        });
+    else
+        eventHandler.emit(eventsList.connectionError.eventName, extendObject({error: 'You need to connect to any port first'}, eventsList.connectionError));
+    
+};
 
 /////////////////////Data
 function sendData(line) {
     if(!line || typeof line != string)
-        return eventEmitter.emit(eventsList.sendDataError.eventName, extendObject({error: '"line" is must defined and need to be a string.'}, eventsList.sendDataError));
+        return eventHandler.emit(eventsList.sendDataError.eventName, extendObject({error: '"line" is must defined and need to be a string.'}, eventsList.sendDataError));
 
     if(connectedPort)
         connectedPort.write(line, err => {
             if(err) 
-                return eventEmitter.emit(eventsList.sendDataError.eventName, extendObject({error: err}, eventsList.sendDataError));
+                return eventHandler.emit(eventsList.sendDataError.eventName, extendObject({error: err}, eventsList.sendDataError));
 
             console.log('SerialComm sendData: ', line);
-            eventEmitter.emit(eventsList.dataSent.name, extendObject({line: line}, eventsList.dataSent));
+            eventHandler.emit(eventsList.dataSent.name, extendObject({line: line}, eventsList.dataSent));
         });
 
     else 
-        return eventEmitter.emit(eventsList.sendDataError.eventName, extendObject({error: err}, eventsList.sendDataError));
+        return eventHandler.emit(eventsList.sendDataError.eventName, extendObject({error: err}, eventsList.sendDataError));
 };
 
 /////////////////////Exports
