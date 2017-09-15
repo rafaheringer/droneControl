99500d55e8ceaf56f4dea4584b28f799f7bc6ccb
Rafael Heringer <rafaheringer@live.com>
2017-09-15 18:26:30 -0300

    removing unnecessary packages

--- NodeProject/config/express.js
+++ NodeProject/config/express.js
@@ -1,22 +1,13 @@
 var express = require('express');
 var glob = require('glob');
 
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compress = require('compression');
 var methodOverride = require('method-override');
 
 module.exports = function(app, config) {
-  //View engine
-  // app.use(favicon(config.root + '/public/img/favicon.ico'));
-  app.use(logger('dev'));
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({extended: true}));
   app.use(compress());
   app.use(express.static(config.root + '/public'));
   app.use('/nodemodules', express.static(config.root + '/node_modules'));
